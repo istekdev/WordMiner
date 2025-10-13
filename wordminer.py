6058c2c9e1044a74288aad564e6041c04681270d
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-10-13 17:23:11 -0500

    Create wordminer.py

---
+++ wordminer.py
@@ -0,0 +1,117 @@
+from termcolor import colored
+from eth_utils import keccak
+import requests
+import json
+import time
+import os
+
+difficulty = 0
+target = 0
+
+if os.path.exists("chain.json"):
+  with open("chain.json", "r") as r:
+    read = json.load(r)
+  newest = list(read.keys())[-1]
+  target = read[newest]["target"]
+  difficulty = (2**244 * (2**32 - 1)) // target
+else:
+  difficulty = 1000000 * 180
+  target = (2**224) * ((2**32) - 1) // difficulty
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def mine():
+  with open("config.json", "r") as r:
+    read = json.load(r)
+  connect = requests.get(read["endpoint"])
+  print(colored(f"Developer: {read["developer"]}", "white", attrs=["bold"]))
+  print(colored(f"Github: {read["github"]}", "white", attrs=["bold"]))
+  created = read["date_of_creation"]
+  local = time.localtime(created)
+  date = time.strftime("%Y-%m-%d %H:%M:%S", local)
+  print(colored(f"Created: {date}", "white", attrs=["bold"]))
+  print("")
+  print(colored("Mining Has Begun...", "yellow", attrs=["bold"]))
+  version = 1
+  max = (2**32) - 1
+  start = round(time.time())
+  currentword = connect[0]
+  timestamp = round(time.time())
+  end = 0
+  nonce = 0
+  while nonce > max:
+    wordhash = keccak(keccak(currentword.encode("utf-8") + target.to_bytes(32, "big") + str(timestamp).encode("utf-8") + nonce.to_bytes(32, "big") + str(end).encode("utf-8") + version.to_bytes(4, "big"))).hex()
+    if int(wordhash, 16) => target:
+      print(colored("Word Has Been Mined, Adding To The Blockchain...", "green", attrs=["bold"]))
+      end = round(time.time())
+      with open("chain.json", "r") as verify:
+        ver = json.load(verify)
+      if ver == {}:
+        block = {
+          "version": version,
+          "timestamp": timestamp,
+          "mined": end,
+          "word": currentword,
+          "nonce": nonce,
+          "target": target,
+          "wordHash": wordhash,
+          "prevHash": "0"*64
+        }
+        with open("chain.json", "w") as add:
+          json.dump(ver, add, indent=4)
+      else:
+        with open("chain.json", "r") as r:
+          read = json.read(r)
+        prev = list(read.keys())[0]
+        prevHash = read[newest]["wordHash"]
+        block = {
+          "version": version,
+          "timestamp": timestamp,
+          "mined": end,
+          "word": currentword,
+          "nonce": nonce,
+          "target": target,
+          "wordHash": wordhash,
+          "prevHash": prevHash
+        }
+        with open("chain.json", "a") as add:
+          json.dump(read, add, indent=4)
+  if nonce < max:
+    print(colored("Nonces Have Been Exhausted - Renewing...", "yellow", attrs=["bold"]))
+    mine()
+  else:
+    nonce +=1
+  with open("chain.json", "r") as r:
+    read = json.load(r)
+  latest = list(read.keys())[-1]
+  start = read[newest]["timestamp"]
+  end = read[newest]["end"]
+  actual = end - start
+  difficulty = difficulty * (180 * actual)
+  target = (2**224) * ((2**32) - 1) // difficulty
+  clear()
+  mine()
+
+def connect():
+  print(colored("Establishing A Connection To The API Endpoint...", "yellow", attrs=["bold"]))
+  with open("config.json", "r") as r:
+    read = json.load(r)
+  connect = requests.get(read["endpoint"])
+  try:
+      word = connect.json()
+  except ValueError:
+      print(colored("Error - Failed To Connect.", "red", attrs=["bold"]))
+      word = None
+  print(colored(f"Successfully Connected - Endpoint: {read['endpoint']}", "green", attrs=["bold"]))
+  if os.path.exists("chain.json"):
+    print(colored("Welcome Back", "green", attrs=["bold"]))
+  else:
+    print(colored("Generating New Blockchain...", "yellow", attrs=["bold"]))
+    with open("chain.json", "w") as new:
+      new.write("{}")
+    print(colored("Your New Blockchain Has Been Generated", "green", attrs=["bold"]))
+  clear()
+  mine()
+
+connect()
